e984e37e86c061fd85b12e513a1e7301179460c3
yanirp <itsjustbusiness987@gmail.com>
2025-06-07 18:46:50 +0300

    Initial commit - add Playwright test for hahishook

---
+++ tests/test_topup.py
@@ -0,0 +1,49 @@
+import pytest
+from playwright.sync_api import sync_playwright
+
+def test_topup():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        page = browser.new_page()
+
+        # 1. נווט לאתר
+        page.goto("https://hahishook.com/")
+        page.wait_for_selector("text=החישוק - מיזם חברתי כלכלי שמשנה את חוקי המשחק")
+        assert page.locator("text=החישוק - מיזם חברתי כלכלי שמשנה את חוקי המשחק").is_visible()
+
+        # לחץ על הקישור "החשבון שלי"
+        page.click("li#menu-item-938 a")
+        response = page.request.get("https://hahishook.com/my-account/")
+        assert response.status == 200
+        assert "החשבון שלי" in response.text()
+
+        # לחץ על כפתור "התחברות עם סיסמה"
+        page.click("//button[contains(text(),'התחברות עם סיסמה')]")
+        page.wait_for_selector("//input[@id='username']")
+        page.wait_for_selector("//input[@id='password']")
+
+        # מלא את פרטי ההתחברות
+        page.fill("//input[@id='username']","yanirqa10@gmail.com")
+        page.fill("//input[@id='password']", "patel0025!")
+        page.click("//button[contains(text(),'שלח שם וסיסמה')]")
+
+
+        page.click("//a[contains(text(),'הארנק שלי')]")
+        response = page.request.get("https://hahishook.com/my-account/woo-wallet/")
+        assert response.status == 200
+        assert "יתרה" in response.text()
+
+        # לחיצה על טעינת כפתור סיבוס
+        page.click("//p[contains(text(),'טעינה עם סיבוס')]")
+        page.wait_for_selector("//input[@id='woo_wallet_balance_to_add']")
+        page.fill("//input[@id='woo_wallet_balance_to_add']", "4")
+        page.wait_for_selector("//input[@value='0']")
+        # בחר שהחישוק ישלם (radio button עם value='0')
+        page.check("//input[@value='0']")
+        # לחץ על כפתור טעינה עם סיבוס (מעבר באותו הטאב)
+        page.click("//button[contains(text(), 'טעינה עם סיבוס')]")
+        # המתן שהכתובת תשתנה ותיטען לכתובת הסיבוס עם הטוקן
+        page.wait_for_url("https://myconsumers.pluxee.co.il/**", wait_until="domcontentloaded")
+        assert "myconsumers.pluxee.co.il" in page.url
+        page.wait_for_selector("//h1[@id='hTitle']")
+        assert page.locator("//h1[@id='hTitle']").is_visible()
